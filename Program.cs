7a1cc591707cc9c3ca9586072cf2f97c2969e7a2
Zach Deibert <zachdeibert@gmail.com>
2018-01-14 16:04:21 -0600

    Created project

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Com.GitHub.ThreePrime.PrimeFinder {
+    static class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
